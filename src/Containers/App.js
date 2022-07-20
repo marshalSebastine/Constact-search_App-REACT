d3ac6beb006628e94a6bcee36fe8dceca1198915
marshal <marshalalakkadan@gmail.com>
2022-07-20 05:59:10 +0530

    before convrting to typescript

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,44 +1,75 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import CardsArray from '../Components/CardArray'
 import SearchBox from '../Components/SearchBox'
 import Scroll  from '../Components/Scroll.js'
 import ErrorBoundary from '../Components/ErrorBoundary.js'
 
 
-class App extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            search: ''
-        }
-    }
-    onSearchChange = (event) => {
+function App(){
+    // constructor(){
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         search: ''
+    //     }
+    // }
+    const [robots,setRobots] = useState([])
+    const [search,setSearch] = useState('')
+    function onSearchChange(event){
 
-        this.setState({search: event.target.value})
+        // this.setState({search: event.target.value})
+        setSearch(event.target.value)
     }
-    componentDidMount(){
+    // componentDidMount(){
 
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then(response => {return response.json()})
+    //     .then(users => {this.setState({robots: users})})
+
+    // }
+    useEffect(() => { 
         fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => {return response.json()})
-        .then(users => {this.setState({robots: users})})
-
-    }
+        .then( response => {return response.json()})
+        .then( users => {setRobots(users)})
+     },[]);
     
-    render(){
-        const {robots,search} = this.state
+    // render(){
+    //     const {robots,search} = this.state
+    //     const filteredrobots = robots.filter(robot => {
+    //         return robot.name.toLowerCase().includes(this.state.search.toLowerCase())
+    //     }
+    //         )
+    //     if (this.state.robots.length === 0){
+    //         return(<h1 className='tc grow'>Loading...</h1>)
+    //     }else{
+    //     return(
+            
+    //         <div className='tc' >
+    //         <h1> RoBo-Friends</h1>
+    //         <SearchBox searchchange = {this.onSearchChange}/>
+    //         <Scroll>
+    //             <ErrorBoundary>
+    //               <CardsArray robots = {filteredrobots}/>
+    //             </ErrorBoundary>
+
+    //         </Scroll>
+            
+    //         </div>
+    //     )}
+    // }
+
         const filteredrobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.search.toLowerCase())
+            return robot.name.toLowerCase().includes(search.toLowerCase())
         }
             )
-        if (this.state.robots.length === 0){
+        if (robots.length === 0){
             return(<h1 className='tc grow'>Loading...</h1>)
         }else{
         return(
             
             <div className='tc' >
             <h1> RoBo-Friends</h1>
-            <SearchBox searchchange = {this.onSearchChange}/>
+            <SearchBox searchchange = {onSearchChange}/>
             <Scroll>
                 <ErrorBoundary>
                   <CardsArray robots = {filteredrobots}/>
@@ -48,7 +79,6 @@ class App extends React.Component{
             
             </div>
         )}
-    }
 
 }
 export default App
